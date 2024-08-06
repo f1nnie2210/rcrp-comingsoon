705bf0b6867516a3b8fe13b63abb3c80e051361a
Mrdua0902 <nqtnikon@gmail.com>
2024-08-06 22:53:19 +0700

    Push

---
+++ funto-nextjs/src/app/[...not-found]/page.tsx
@@ -0,0 +1,14 @@
+import Error from "@/components/error";
+
+export const metadata = {
+	title: "404 error || RC:RP GTA:SA Roleplay Việt Nam",
+};
+const index = () => {
+	return (
+		<>
+			<Error />
+		</>
+	);
+};
+
+export default index;
---
+++ funto-nextjs/src/app/page.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import HomeOne from "@/components/homes/home";
+
+export const metadata = {
+	title: "GTA:SA Red Commnunity Roleplay",
+};
+
+const index = () => {
+	return (
+		<>
+			<HomeOne />
+		</>
+	);
+};
+
+export default index;
